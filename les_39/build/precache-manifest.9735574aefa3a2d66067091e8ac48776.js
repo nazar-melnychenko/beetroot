43f0d968f22c94e280b4b4d2ce8b26cecf5ed49a
Nazar Melnychenko <nazar.melnychenko@gmail.com>
2020-02-04 23:23:02 +0200

    Added React

---
+++ les_39/build/precache-manifest.9735574aefa3a2d66067091e8ac48776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb6323c9cb4a5ed43ede797d979a5a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81afb590f85412528b1e",
+    "url": "/static/css/main.05240399.chunk.css"
+  },
+  {
+    "revision": "606f2592ab7994223e8a",
+    "url": "/static/js/2.bde889f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bde889f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81afb590f85412528b1e",
+    "url": "/static/js/main.e11de855.chunk.js"
+  },
+  {
+    "revision": "5ea09d23903641a24c90",
+    "url": "/static/js/runtime-main.9fe1a42c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
