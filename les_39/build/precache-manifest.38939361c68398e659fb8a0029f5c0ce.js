b1c6805599c822739edf7e0d7b652941f72e9a44
Nazar Melnychenko <nazar.melnychenko@gmail.com>
2020-02-11 01:02:33 +0200

    todolist

---
+++ les_39/build/precache-manifest.38939361c68398e659fb8a0029f5c0ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cf2eb07980be6e5510aa85ed28f7f17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da5d602ed502235bd088",
+    "url": "/static/css/main.ad44d56a.chunk.css"
+  },
+  {
+    "revision": "7794ef828925ffff8086",
+    "url": "/static/js/2.4e8f8fe3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4e8f8fe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5d602ed502235bd088",
+    "url": "/static/js/main.749b88ec.chunk.js"
+  },
+  {
+    "revision": "5ea09d23903641a24c90",
+    "url": "/static/js/runtime-main.9fe1a42c.js"
+  }
+]);
